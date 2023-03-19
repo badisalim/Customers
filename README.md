f3e81dfbdd94ba6d8a09dcd50e5d4a327897f22f
badisalim <44968319+badisalim@users.noreply.github.com>
2023-03-19 13:24:06 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Customers
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/github-pjdcnw-xjkqug)
---
+++ src/app/customer/customer.ts
@@ -0,0 +1,8 @@
+export interface Customer {
+  id: number;
+  firstname: string;
+  name: string;
+  country: string;
+  birthdate: string;
+  mobilenumber: number;
+}
